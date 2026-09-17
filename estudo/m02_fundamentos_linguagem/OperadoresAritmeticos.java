c30da0e3e0daab3cd0f3d0f80697b30cb32b7897
Walace Delgado <walacecastro@live.com>
2026-09-17 13:43:48 -0300

    fundamentos(aula): OperadoresAritmeticos e TrabalhandoVariaveis.

---
+++ estudo/m02_fundamentos_linguagem/OperadoresAritmeticos.java
@@ -0,0 +1,31 @@
+void main(){
+    int minhaIdade = 50;
+    int suaIdade = 30;
+
+    // Adição
+    int somaIdades = minhaIdade + suaIdade;
+
+    // Subtração
+    int difIdades = minhaIdade - suaIdade;
+
+    // Multiplicação
+    int multIdades = minhaIdade * minhaIdade;
+
+    // Divisão
+    int divIdade = suaIdade / minhaIdade;
+
+    // Modulo Divisão
+    int restoDivisao = 7%2;
+
+    IO.println("Adição: " + somaIdades);
+    IO.println("Subtração: " + difIdades);
+    IO.println("Multiplicação: " + multIdades);
+    IO.println("divisão: " + difIdades);
+    IO.println("Resto da Divisão: " + restoDivisao);
+
+
+
+
+
+
+}
---
+++ estudo/m02_fundamentos_linguagem/TrabalhandoVariaveis.java
@@ -0,0 +1,13 @@
+
+void main() {
+    int numeroInteiro;
+    int anoNascimento;
+
+    numeroInteiro = 10;
+    anoNascimento = 1990;
+
+    int soma = numeroInteiro + anoNascimento;
+
+    IO.println("soma: " + soma);
+
+}
