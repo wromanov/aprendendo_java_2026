8fa3a74a66f946c1aedbb15480d623991d1d7a73
Walace Delgado <walacecastro@live.com>
2026-09-17 14:53:06 -0300

    fundamentos(aula): OperadoresAbreviacao e OperadoresIncremento

---
+++ estudo/m02_fundamentos_linguagem/OperadoresAbreviacao.java
@@ -0,0 +1,19 @@
+
+void main() {
+
+    int qtdProduto = 10;
+
+    qtdProduto += 1;
+
+    qtdProduto -= 1;
+
+    qtdProduto *= 2;
+
+    qtdProduto /= 2;
+
+    qtdProduto %= 2;
+
+
+    IO.println(qtdProduto);
+
+}
---
+++ estudo/m02_fundamentos_linguagem/OperadoresIncremento.java
@@ -0,0 +1,14 @@
+
+void main(){
+    int qtdDownload = 0;
+
+    //Operador Incremento
+    qtdDownload ++;
+    qtdDownload ++;
+    qtdDownload ++;
+
+    //Operador Decremento
+    qtdDownload --;
+
+    System.out.println(qtdDownload);
+}
