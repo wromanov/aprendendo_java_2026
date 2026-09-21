961a5cff050dd23e147c6ed35ff9674989b7710f
Walace Delgado <walacecastro@live.com>
2026-09-21 18:00:45 -0300

    fundamentos(aula): casting tipos primitivos.

---
+++ estudo/m02_fundamentos_linguagem/CastingPrimitivos.java
@@ -0,0 +1,19 @@
+void main() {
+
+    // Casting long para int
+
+    long numLong = 100L;
+
+    int castLong = (int) numLong;
+
+    IO.println(castLong);
+
+    // Casting Double para Float, sendo que vai perder precisão de casa decimal.
+
+    double numDouble = 1.7976956898454123;
+
+    float castDouble = (float) numDouble;
+
+    IO.println(castDouble);
+
+}
