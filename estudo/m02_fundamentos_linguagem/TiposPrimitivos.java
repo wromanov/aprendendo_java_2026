5e319d3d0b821dd23747741705bdd701dbdd051f
Walace Delgado <walacecastro@live.com>
2026-09-21 15:06:21 -0300

    fundamentos(aula): tipos primitivos.

--- estudo/m02_fundamentos_linguagem/TiposPrimitivos.java
+++ estudo/m02_fundamentos_linguagem/TiposPrimitivos.java
@@ -21,7 +21,38 @@ void main() {
     IO.println("Inicial do seu nome: " + inicialNome);
 
     //Tipo byte
-    byte valorMaximo = 127;
-    byte valorMinimo = -128;
+    byte maiorValorByte = 127;
+    byte menorValorByte = -128;
+
+    //Tipo short
+    short maiorValorShort = 32767;
+    short menorValorShort = -32768;
+
+    //Tipo int
+    int maiorValorInt = 2_147_483_647;
+    int menorValorInt = -2_147_483_648;
+
+    //Tipo long
+    long maiorValorLong = 9_223_372_036_854_775_807L;
+    long menorValorLong = -9_223_372_036_854_775_808L;
+
+    //Tipo float
+    float maiorValorFloat = 3.4028235E38F;
+    float menorValorFloat = -3.4028235E38F;
+
+    //Tipo double
+    double maiorValorDouble = 1.7976931348623157E308;
+    double menorValorDouble = -1.7976931348623157E308;
+
+    // Inteiros são int por padrão, mesmo declarando a variável como long.
+    // Use L para indicar que o literal é do tipo long,
+    // principalmente quando o valor ultrapassa o limite de int.
+    // Ex. long numero = 3_000_000_000L;
+
+    // Números decimais são double por padrão.
+    // Use F para indicar que o literal é do tipo float, mesmo a declarado como float.
+    // Ex. float preço = 10.5F;
+
+
 
 }
